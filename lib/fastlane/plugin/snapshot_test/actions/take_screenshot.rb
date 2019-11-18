bde15f2fb87946e78be81e91b5c82486ec60dc33
wasabeef <dadadada.chop@gmail.com>
2019-11-18 14:43:44 +0900

    fix param and syntax for new test lab plugin

--- lib/fastlane/plugin/snapshot_test/actions/take_screenshot.rb
+++ lib/fastlane/plugin/snapshot_test/actions/take_screenshot.rb
@@ -6,8 +6,8 @@ module Fastlane
       def self.run(params)
         download_dir = params[:download_dir]
 
-        firebase_test_lab_results_bucket = params[:firebase_test_lab_results_bucket] == nil ? "#{params[:project_id]}_test_results" : params[:firebase_test_lab_results_bucket]
-        firebase_test_lab_results_dir = "firebase_screenshot_#{DateTime.now.strftime('%Y-%m-%d-%H:%M:%S')}"
+        results_bucket = params[:firebase_test_lab_results_bucket] ? "#{params[:project_id]}_test_results" : params[:firebase_test_lab_results_bucket]
+        results_dir = "firebase_screenshot_#{DateTime.now.strftime('%Y-%m-%d-%H:%M:%S')}"
         devices = params[:devices]
         Fastlane::Actions::FirebaseTestLabAndroidAction.run(
             project_id: params[:project_id],
@@ -18,14 +18,16 @@ module Fastlane
             app_test_apk: params[:app_test_apk],
             console_log_file_name: "#{download_dir}/firebase_os_test_console.log",
             timeout: params[:timeout],
-            extra_options: "--results-bucket #{firebase_test_lab_results_bucket} --results-dir #{firebase_test_lab_results_dir} --no-record-video"
+            firebase_test_lab_results_bucket: results_bucket,
+            firebase_test_lab_results_dir: results_dir,
+            extra_options: "--no-record-video"
         )
 
         UI.message "Fetch screenshots from Firebase Test Lab results bucket"
         device_names = devices.map(&method(:device_name))
         device_names.each do |device_name|
           `mkdir -p #{download_dir}/#{device_name}`
-          Action.sh "gsutil -m rsync -d -r gs://#{firebase_test_lab_results_bucket}/#{firebase_test_lab_results_dir}/#{device_name}/artifacts #{download_dir}/#{device_name}"
+          Action.sh "gsutil -m rsync -d -r gs://#{results_bucket}/#{results_dir}/#{device_name}/artifacts #{download_dir}/#{device_name}"
           `rm -rf #{download_dir}/#{device_name}/sdcard`
 
           entries = Dir::entries("#{download_dir}/#{device_name}").select { |entry| entry =~ /^.*\.(jpg|jpeg|png)/ }
@@ -94,7 +96,8 @@ module Fastlane
                                          env_name: "FIREBASE_TEST_LAB_RESULTS_BUCKET",
                                          description: "Name of Firebase Test Lab results bucket",
                                          type: String,
-                                         optional: true),
+                                         optional: true,
+                                         default_value: nil),
             FastlaneCore::ConfigItem.new(key: :download_dir,
                                          env_name: "DOWNLOAD_DIR",
                                          description: "Target directory to download screenshots from firebase",
@@ -140,7 +143,7 @@ module Fastlane
             ],
             app_apk: "tools/app-snapshot-debug.apk",
             app_test_apk: "app/build/outputs/apk/androidTest/snapshot/debug/installedapp-snapshot-debug-androidTest.apk",
-            firebase_test_lab_results_bucket: "cats-android-firebase-instrumented-test",
+            results_bucket: "cats-android-firebase-instrumented-test",
             timeout: "10m",
             download_dir: ".screenshot"
         )']
