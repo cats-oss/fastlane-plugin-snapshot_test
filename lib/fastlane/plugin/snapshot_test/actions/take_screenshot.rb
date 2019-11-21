7e29f232d42e16bcc6096fbdfdcd71e8c297b3e2
wasabeef <dadadada.chop@gmail.com>
2019-11-22 01:59:32 +0900

    rubocop format

--- lib/fastlane/plugin/snapshot_test/actions/take_screenshot.rb
+++ lib/fastlane/plugin/snapshot_test/actions/take_screenshot.rb
@@ -10,30 +10,30 @@ module Fastlane
         results_dir = "firebase_screenshot_#{DateTime.now.strftime('%Y-%m-%d-%H:%M:%S')}"
         devices = params[:devices]
         Fastlane::Actions::FirebaseTestLabAndroidAction.run(
-            project_id: params[:project_id],
-            gcloud_service_key_file: params[:gcloud_service_key_file],
-            type: "instrumentation",
-            devices: devices,
-            app_apk: params[:app_apk],
-            app_test_apk: params[:app_test_apk],
-            console_log_file_name: "#{download_dir}/firebase_os_test_console.log",
-            timeout: params[:timeout],
-            firebase_test_lab_results_bucket: results_bucket,
-            firebase_test_lab_results_dir: results_dir,
-            extra_options: "--no-record-video"
+          project_id: params[:project_id],
+          gcloud_service_key_file: params[:gcloud_service_key_file],
+          type: "instrumentation",
+          devices: devices,
+          app_apk: params[:app_apk],
+          app_test_apk: params[:app_test_apk],
+          console_log_file_name: "#{download_dir}/firebase_os_test_console.log",
+          timeout: params[:timeout],
+          firebase_test_lab_results_bucket: results_bucket,
+          firebase_test_lab_results_dir: results_dir,
+          extra_options: "--no-record-video"
         )
 
-        UI.message "Fetch screenshots from Firebase Test Lab results bucket"
+        UI.message("Fetch screenshots from Firebase Test Lab results bucket")
         device_names = devices.map(&method(:device_name))
         device_names.each do |device_name|
           `mkdir -p #{download_dir}/#{device_name}`
-          Action.sh "gsutil -m rsync -d -r gs://#{results_bucket}/#{results_dir}/#{device_name}/artifacts #{download_dir}/#{device_name}"
+          Action.sh("gsutil -m rsync -d -r gs://#{results_bucket}/#{results_dir}/#{device_name}/artifacts #{download_dir}/#{device_name}")
           `rm -rf #{download_dir}/#{device_name}/sdcard`
 
-          entries = Dir::entries("#{download_dir}/#{device_name}").select { |entry| entry =~ /^.*\.(jpg|jpeg|png)/ }
+          entries = Dir.entries("#{download_dir}/#{device_name}").select { |entry| entry =~ /^.*\.(jpg|jpeg|png)/ }
           for entry in entries do
             filePath = "#{download_dir}/#{device_name}/#{entry}"
-            Action.sh "convert #{filePath} -scale 320x #{filePath}"
+            Action.sh("convert #{filePath} -scale 320x #{filePath}")
           end
         end
       end
@@ -52,57 +52,57 @@ module Fastlane
 
       def self.available_options
         [
-            FastlaneCore::ConfigItem.new(key: :project_id,
-                                         env_name: "PROJECT_ID",
-                                         description: "Your Firebase project id",
-                                         type: String,
-                                         optional: false),
-            FastlaneCore::ConfigItem.new(key: :gcloud_service_key_file,
-                                         env_name: "GCLOUD_SERVICE_KEY_FILE",
-                                         description: "File path containing the gcloud auth key. Default: Created from GCLOUD_SERVICE_KEY environment variable",
-                                         type: String,
-                                         optional: false),
-            FastlaneCore::ConfigItem.new(key: :devices,
-                                         env_name: "DEVICES",
-                                         description: "Devices to test the app on",
-                                         type: Array,
-                                         verify_block: proc do |value|
-                                           UI.user_error!("Devices have to be at least one") if value.empty?
-                                           value.each do |device|
-                                             check_has_property(device, :model)
-                                             check_has_property(device, :version)
-                                             set_default_property(device, :locale, "en_US")
-                                             set_default_property(device, :orientation, "portrait")
-                                           end
-                                         end),
-            FastlaneCore::ConfigItem.new(key: :timeout,
-                                         env_name: "TIMEOUT",
-                                         description: "The max time this test execution can run before it is cancelled. Default: 5m (this value must be greater than or equal to 1m)",
-                                         type: String,
-                                         optional: true,
-                                         default_value: "5m"),
-            FastlaneCore::ConfigItem.new(key: :app_apk,
-                                         env_name: "APP_APK",
-                                         description: "The path for your android app apk",
-                                         type: String,
-                                         optional: false),
-            FastlaneCore::ConfigItem.new(key: :app_test_apk,
-                                         env_name: "APP_TEST_APK",
-                                         description: "The path for your android test apk. Default: empty string",
-                                         type: String,
-                                         optional: true,
-                                         default_value: nil),
-            FastlaneCore::ConfigItem.new(key: :firebase_test_lab_results_bucket,
-                                         env_name: "FIREBASE_TEST_LAB_RESULTS_BUCKET",
-                                         description: "Name of Firebase Test Lab results bucket",
-                                         type: String,
-                                         optional: true,
-                                         default_value: nil),
-            FastlaneCore::ConfigItem.new(key: :download_dir,
-                                         env_name: "DOWNLOAD_DIR",
-                                         description: "Target directory to download screenshots from firebase",
-                                         type: String,
-                                         optional: false)
+          FastlaneCore::ConfigItem.new(key: :project_id,
+                                       env_name: "PROJECT_ID",
+                                       description: "Your Firebase project id",
+                                       type: String,
+                                       optional: false),
+          FastlaneCore::ConfigItem.new(key: :gcloud_service_key_file,
+                                       env_name: "GCLOUD_SERVICE_KEY_FILE",
+                                       description: "File path containing the gcloud auth key. Default: Created from GCLOUD_SERVICE_KEY environment variable",
+                                       type: String,
+                                       optional: false),
+          FastlaneCore::ConfigItem.new(key: :devices,
+                                       env_name: "DEVICES",
+                                       description: "Devices to test the app on",
+                                       type: Array,
+                                       verify_block: proc do |value|
+                                         UI.user_error!("Devices have to be at least one") if value.empty?
+                                         value.each do |device|
+                                           check_has_property(device, :model)
+                                           check_has_property(device, :version)
+                                           set_default_property(device, :locale, "en_US")
+                                           set_default_property(device, :orientation, "portrait")
+                                         end
+                                       end),
+          FastlaneCore::ConfigItem.new(key: :timeout,
+                                       env_name: "TIMEOUT",
+                                       description: "The max time this test execution can run before it is cancelled. Default: 5m (this value must be greater than or equal to 1m)",
+                                       type: String,
+                                       optional: true,
+                                       default_value: "5m"),
+          FastlaneCore::ConfigItem.new(key: :app_apk,
+                                       env_name: "APP_APK",
+                                       description: "The path for your android app apk",
+                                       type: String,
+                                       optional: false),
+          FastlaneCore::ConfigItem.new(key: :app_test_apk,
+                                       env_name: "APP_TEST_APK",
+                                       description: "The path for your android test apk. Default: empty string",
+                                       type: String,
+                                       optional: true,
+                                       default_value: nil),
+          FastlaneCore::ConfigItem.new(key: :firebase_test_lab_results_bucket,
+                                       env_name: "FIREBASE_TEST_LAB_RESULTS_BUCKET",
+                                       description: "Name of Firebase Test Lab results bucket",
+                                       type: String,
+                                       optional: true,
+                                       default_value: nil),
+          FastlaneCore::ConfigItem.new(key: :download_dir,
+                                       env_name: "DOWNLOAD_DIR",
+                                       description: "Target directory to download screenshots from firebase",
+                                       type: String,
+                                       optional: false)
         ]
       end
 
@@ -113,7 +113,6 @@ module Fastlane
       def self.is_supported?(platform)
         platform == :android
       end
-
 
       def self.check_has_property(hash_obj, property)
         UI.user_error!("Each device must have #{property} property") unless hash_obj.key?(property)
@@ -148,7 +147,6 @@ module Fastlane
             download_dir: ".screenshot"
         )']
       end
-
     end
   end
 end
